616a542c8ce9b4c2468149d94b1ba6e2ac0d8b59
Luiz Palma <luizpalma16@gmail.com>
2024-07-08 20:49:51 -0300

    Consultar Chamado

--- php/registra_chamado.php
+++ php/registra_chamado.php
@@ -1,18 +1,29 @@
 <?php
+session_start();
 define('SEPARADOR', " | ");
 define('SEP_ALTERNATIVO', " - ");
+$_SESSION['SEPARADOR'] = SEPARADOR;
+
+function recebe_e_ProcessaDados()
+{
+    $titulo = removeSeparador($_POST['titulo']);
+    $categoria = removeSeparador($_POST['categoria']);
+    $descricao = removeSeparador($_POST['descricao']);
+    $texto = $titulo . SEPARADOR . $categoria .  SEPARADOR . $descricao . PHP_EOL;
+    return $texto;
+}
+
 function removeSeparador($str, $separador = SEPARADOR, $sep_alternativo = SEP_ALTERNATIVO)
 {
     return str_replace(SEPARADOR, SEP_ALTERNATIVO, $str);
 }
 
-$titulo = removeSeparador($_POST['titulo']);
-$categoria = removeSeparador($_POST['categoria']);
-$descricao = removeSeparador($_POST['descricao']);
-$texto = $titulo . SEPARADOR . $categoria .  SEPARADOR . $descricao . PHP_EOL;
+function gravaDados($nome_arquivo, $texto)
+{
+    $arquivo = fopen($nome_arquivo, 'a');
+    fwrite($arquivo, $texto);
+    fclose($arquivo);
+}
 
-$arquivo = fopen('arquivo.hd', 'a');
-fwrite($arquivo, $texto);
-fclose($arquivo);
-
+gravaDados('arquivo.hd', recebe_e_ProcessaDados());
 header('Location: abrir_chamado.php');
